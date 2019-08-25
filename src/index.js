83e3d842c65718d5224690414dcb2ad46c22d90d
unknown <kirill.krupina26@gmail.com>
2019-08-25 20:03:28 +0300

    delete goods from cart

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 
     // CHECKBOX
 
-const checkbox = document.querySelectorAll('.filter-check_checkbox');
+function toggleCheckbox() {
+    const checkbox = document.querySelectorAll('.filter-check_checkbox');
 
 checkbox.forEach((element) => {
     element.addEventListener('change', function() {
@@ -14,51 +15,109 @@ checkbox.forEach((element) => {
         }
     });
 });
+}   
 
     // END CHECKBOX
 
+
     // CART
 
-const 
-    buttonCart = document.getElementById('cart'),
-    modalCart = document.querySelector('.cart'),
-    closeButtonCart = document.querySelector('.cart-close');
+function toggleCart() {
+    const 
+        buttonCart = document.getElementById('cart'),
+        modalCart = document.querySelector('.cart'),
+        closeButtonCart = document.querySelector('.cart-close');
 
-buttonCart.addEventListener('click', () => {
-    modalCart.style.display = 'flex';
-    document.body.style.overflow  = 'hidden';
-});
+    buttonCart.addEventListener('click', () => {
+        modalCart.style.display = 'flex';
+        document.body.style.overflow  = 'hidden';
+    });
 
-closeButtonCart.addEventListener('click', () => {
-    modalCart.style.display = '';
-    document.body.style.overflow  = '';
-})
+    closeButtonCart.addEventListener('click', () => {
+        modalCart.style.display = '';
+        document.body.style.overflow  = '';
+    })
 
+}
 
     // END CART 
 
 
-    // GOODS 
+    // GOODS IN CART
 
-const 
-    cards = document.querySelectorAll('.goods .card'),
-    cartWrapper = document.querySelector('.cart-wrapper'),
-    cartEmpty = document.getElementById('cart-empty'),
-    cartCounter = document.querySelector('.counter');
+function addCart() {
+    const 
+        cards = document.querySelectorAll('.goods .card'),
+        cartWrapper = document.querySelector('.cart-wrapper'),
+        cartEmpty = document.getElementById('cart-empty'),
+        cartCounter = document.querySelector('.counter');
 
-cards.forEach((card) => {
-    const btn = card.querySelector('.btn');
-    btn.addEventListener('click', () => {
-        const cardClone = card.cloneNode(true);
-        cartWrapper.appendChild(cardClone);
-        cartEmpty.remove();
-        showData();
+    cards.forEach((card) => {
+        const btn = card.querySelector('.btn');
+
+        btn.addEventListener('click', () => {
+            const cardClone = card.cloneNode(true);
+            cartWrapper.appendChild(cardClone);
+            showData();
+
+            const removeBtn = cardClone.querySelector('.btn');
+            removeBtn.textContent = 'Удалить из корзины';
+            removeBtn.addEventListener('click', () => {
+                cardClone.remove();
+                showData();
+            })
+        });
     });
-});
 
-function showData() {
-    const cardsCart = cartWrapper.querySelectorAll('.card');
-    cartCounter.textContent = cardsCart.length;
+    function showData() {
+        const 
+            cardsCart = cartWrapper.querySelectorAll('.card'),
+            cardsPrice = cartWrapper.querySelectorAll('.card-price'),
+            cardTotal = document.querySelector('.cart-total span');
+
+        let priceSum = 0;
+            
+        cartCounter.textContent = cardsCart.length;
+
+        cardsPrice.forEach((cardPrice) => {
+            let price = parseFloat(cardPrice.textContent);
+            priceSum += price;   
+        });
+
+        cardTotal.textContent = priceSum; 
+
+        if(cardsCart.length !== 0) {
+            cartEmpty.remove();
+        } else {
+            cartWrapper.appendChild(cartEmpty);
+        }
+    }
+}    
+
+    // END GOODS IN CART
+
+
+    // FILTER
+
+function actionPage() {
+
+    const 
+        cards = document.querySelectorAll('.goods .card'),
+        saleCheckbox = document.getElementById('discount-checkbox');
+
+    // saleCheckbox.addEventListener('click', () => {
+    //     cards.forEach((card) => {
+            
+    //     })
+    // });    
 }
 
-    // END GOODS  
+    // END FILTER
+
+
+toggleCheckbox();
+toggleCart();
+addCart();
+actionPage();
+
+    
